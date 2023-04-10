90617d416bc70ada1f92148d87f2fda011e4ba3b
Renato Cordova <ren99cordova@gmail.com>
2023-04-10 19:29:01 -0400

    made changes to nav style, slowly changing the overall design of the site

--- src/Components/NavTabs.jsx
+++ src/Components/NavTabs.jsx
@@ -4,41 +4,46 @@ import Navbar from "react-bootstrap/Navbar";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <Container className="nav-bg">
-      <Navbar expand="lg" className="nav-bgr" variant="light">
-        <Container>
-          <Nav className="me-auto">
-            <Navbar.Brand href="#home">RC</Navbar.Brand>
+    <Navbar expand="lg" className="nav-bg" variant="light">
+      <Container>
+        <Nav className="me-auto">
+          <Navbar.Brand href="#home">RC</Navbar.Brand>
+        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className="nav-link"
+              onClick={() => handlePageChange("Home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className="nav-link"
+              onClick={() => handlePageChange("Projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className="nav-link"
+              onClick={() => handlePageChange("Contact")}
+            >
+              Contact Me
+            </Nav.Link>
+            <Nav.Link
+              href="https://docs.google.com/document/d/10bxzqPD-L65BA9RFk4bXjOz0d2Gdjd_t/edit?usp=sharing&ouid=106751600692304358518&rtpof=true&sd=true"
+              target="_blank"
+              className="nav-link"
+            >
+              Resume
+            </Nav.Link>
           </Nav>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" onClick={() => handlePageChange("Home")}>
-                Home
-              </Nav.Link>
-              <Nav.Link
-                href="#projects"
-                onClick={() => handlePageChange("Projects")}
-              >
-                Projects
-              </Nav.Link>
-              <Nav.Link
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-              >
-                Contact Me
-              </Nav.Link>
-              <Nav.Link
-                href="https://docs.google.com/document/d/10bxzqPD-L65BA9RFk4bXjOz0d2Gdjd_t/edit?usp=sharing&ouid=106751600692304358518&rtpof=true&sd=true"
-                target="_blank"
-              >
-                Resume
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Container>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
