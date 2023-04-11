6faadcbee772b117ef736665c362b99a10febe25
Renato Cordova <ren99cordova@gmail.com>
2023-04-11 15:57:50 -0400

    changed Technologies layour and renamed it to Skills, added font, and swipe feature

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -14,76 +14,97 @@ import {
 } from "react-icons/di";
 import { GrGraphQl } from "react-icons/gr";
 import { SiJest, SiHeroku } from "react-icons/si";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
 
 export default function Technologies() {
   return (
-    <div id="skills" className="tech-container slide-in-top">
-      <h2 className="tech-title">Technologies</h2>
-      <div className="tech-groups">
-        <div className="tech-class">
-          <h3>Font-End</h3>
-          <ul className="tech-icons">
-            <li>
-              <DiHtml5 className="html" title="HTML" />
-            </li>
-            <li>
-              <DiCss3 className="css" title="CSS" />
-            </li>
-            <li>
-              <DiJsBadge className="javascript" title="JavaScript" />
-            </li>
-          </ul>
-          <ul className="tech-icons">
-            <li>
-              <DiReact className="react" title="React" />
-            </li>
-            <li>
-              <DiJqueryLogo className="jquery" title="jQuery" />
-            </li>
-            <li>
-              <DiBootstrap className="bootstrap" title="Bootstrap" />
-            </li>
-          </ul>
-        </div>
-        <div className="tech-class">
-          <h3>Back-end</h3>
-          <ul className="tech-icons">
-            <li>
-              <DiNodejs className="nodejs" title="Node.js" />
-            </li>
-            <li>
-              <DiNpm className="npm" title="npm" />
-            </li>
-            <li>
-              <DiMysql className="mysql" title="mySQL" />
-            </li>
-          </ul>
-          <ul className="tech-icons">
-            <li>
-              <DiMongodb className="mongodb" title="MongoDB" />
-            </li>
-            <li>
-              <GrGraphQl className="graphql" title="GraphQL" />
-            </li>
-          </ul>
-        </div>
-        <div className="tech-class">
-          <h3>Developer Tools</h3>
-          <ul className="tech-icons">
-            <li>
-              <DiGit className="git" title="Git" />
-            </li>
-            <li>
-              <SiJest className="jest" title="Jest" />
-            </li>
-          </ul>
-          <ul className="tech-icons">
-            <li>
-              <SiHeroku className="heroku" title="Heroku" />
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div id="skills" className="skills-container">
+      <h1 className="skills-title">Skills</h1>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="tech-class">
+            <h3>Front-End</h3>
+            <ul className="tech-icons">
+              <li>
+                <DiHtml5 className="html" title="HTML" />
+              </li>
+              <li>
+                <DiCss3 className="css" title="CSS" />
+              </li>
+              <li>
+                <DiJsBadge className="javascript" title="JavaScript" />
+              </li>
+            </ul>
+            <ul className="tech-icons">
+              <li>
+                <DiReact className="react" title="React" />
+              </li>
+              <li>
+                <DiJqueryLogo className="jquery" title="jQuery" />
+              </li>
+              <li>
+                <DiBootstrap className="bootstrap" title="Bootstrap" />
+              </li>
+            </ul>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="tech-class">
+            <h3>Back-end</h3>
+            <ul className="tech-icons">
+              <li>
+                <DiNodejs className="nodejs" title="Node.js" />
+              </li>
+              <li>
+                <DiNpm className="npm" title="npm" />
+              </li>
+              <li>
+                <DiMysql className="mysql" title="mySQL" />
+              </li>
+            </ul>
+            <ul className="tech-icons">
+              <li>
+                <DiMongodb className="mongodb" title="MongoDB" />
+              </li>
+              <li>
+                <GrGraphQl className="graphql" title="GraphQL" />
+              </li>
+            </ul>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="tech-class">
+            <h3>Developer Tools</h3>
+            <ul className="tech-icons">
+              <li>
+                <DiGit className="git" title="Git" />
+              </li>
+              <li>
+                <SiJest className="jest" title="Jest" />
+              </li>
+            </ul>
+            <ul className="tech-icons">
+              <li>
+                <SiHeroku className="heroku" title="Heroku" />
+              </li>
+            </ul>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
